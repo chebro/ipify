708730c33b816a1f8f9d1da97a4c117e4ae8583d
Sravanth C <54331348+chebro@users.noreply.github.com>
2021-05-19 01:42:38 +0530

    fix write after end + redirect invalid reqs

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ const port = process.env.PORT;
 const addr = process.env.ADDR;
 
 app.get('/', (req, res) => {
-  if (req.headers['x-real-ip']) res.end(req.headers['x-real-ip']);
-  res.end(req.ip);
+  if (req.headers['x-real-ip']) {
+    res.status(200).end(req.headers['x-real-ip']);
+    return;
+  }
+  res.status(200).end(req.ip);
 });
+
+app.get('*', (req, res) => {
+  res.status(308).redirect('/');
+})
 
 app.listen(port, addr, () => {
   console.log(`listening on ${addr}:${port}`);
