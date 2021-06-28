db42025cc78f25db2b7d56e17fbfe6d193ded7c6
Sravanth C <54331348+chebro@users.noreply.github.com>
2021-06-28 22:38:41 +0530

    add: Content-Type headers

--- index.js
+++ index.js
@@ -2,21 +2,27 @@ const express = require('express');
 const app = express();
 
 const port = process.env.PORT;
-const addr = process.env.ADDR;
+const addr = process.env.HOST;
 
 app.get('/', (req, res) => {
-  if (req.headers['x-real-ip']) {
-    res.status(200).end(req.headers['x-real-ip']);
-    return;
-  }
-  res.status(200).end(req.ip);
+	if (req.headers['x-real-ip']) {
+		res
+			.status(200)
+			.set('Content-Type', 'text/plain')
+			.end(req.headers['x-real-ip'], 'utf8');
+		return;
+	}
+	res
+		.status(200)
+		.set('Content-Type', 'text/plain')
+		.end(req.ip, 'utf-8');
 });
 
-app.get('*', (req, res) => {
-  res.status(308).redirect('/');
-})
+app.get('*', (_req, res) => {
+	res.status(308).redirect('/');
+});
 
 app.listen(port, addr, () => {
-  console.log(`listening on ${addr}:${port}`);
-})
+	console.log(`listening on ${addr}:${port}`);
+});
 
